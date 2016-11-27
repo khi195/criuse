6e0474d6cdd55dd52ea0e86665b24955902033a6
Kashif Iqbal <Kashif.h.iqbal@gmail.com>
2016-11-27 19:28:48 +0000

    removed path to angular JS test

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,20 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-
+// assign the cruise controller
 var ctrlCruises = require('../controllers/cruises.controller.js')
 
-
+// list all the cruises
 router
   .route('/cruises')
   .get( ctrlCruises.cruises);
-
+// list cruise by destination
 router
   .route('/cruises/:destination')
   .get( ctrlCruises.cruisesDestination);
-
-router
-  .route('/angular')
-  .get( ctrlCruises.angular);
 
 module.exports = router;
