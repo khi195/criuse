39f628d9095aa9e28b409ed1c9a61c0f5ff72b76
Kashif Iqbal <Kashif.h.iqbal@gmail.com>
2016-11-27 19:28:03 +0000

    removed path to test home

--- app.js
+++ app.js
@@ -5,15 +5,9 @@ var routes = require('./api/routes');
 
 app.set('port', 3000);
 
-app.get('/',  function(req, res){
-res.
-    status(200).
-    json('Hello from cruise app');
-});
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', routes);
-
-app.use('/home', express.static(path.join(__dirname, 'public')))
 
 var server = app.listen(app.get('port'),  function(){
 console.log("Listening to port on ", app.get('port'));
